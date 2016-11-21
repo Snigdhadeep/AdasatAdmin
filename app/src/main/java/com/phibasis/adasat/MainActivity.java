22576f9b979ce01c76e09abe0c440a50edac189c
snigdhadeep <snigdhadeep93@gmail.com>
2016-11-21 11:40:53 -0800

    first commit

---
+++ app/src/main/java/com/phibasis/adasat/MainActivity.java
@@ -0,0 +1,21 @@
+package com.phibasis.adasat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.webkit.WebView;
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview=(WebView)findViewById(R.id.web);
+
+
+        webview.loadUrl("https://www.youtube.com");
+
+}
+}
